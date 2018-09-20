bb9ae8305da5970f827fbe4cd9ac7cc94a17700d
Verisana <1902Grand@gmail.com>
2018-09-21 00:30:22 +0500

    New restart

--- restart.sh
+++ restart.sh
@@ -1,4 +1,4 @@
 systemctl restart nginx
 systemctl restart uwsgi
-pkill -f /home/leo/binance_ml/manage.py connect_binance
+pkill -f /home/leo/binance_ml/manage.py
 nohup python3 /home/leo/binance_ml/manage.py connect_binance --type=all_ticker &
